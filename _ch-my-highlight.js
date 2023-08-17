b9a841869a1e02eac9c7f0bf8b7a7d8b0d041d6a
Nguyen Truong An <79207918+flashshare@users.noreply.github.com>
2023-08-17 16:59:54 +0200

    add files

---
+++ _ch-my-highlight.js
@@ -0,0 +1,59 @@
+async function loadScript(url) {
+  return new Promise((resolve, reject) => {
+    // Adding the script tag to the head as suggested before
+    let head = document.head;
+    let script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.src = url;
+    script.async = false;
+
+    script.onload = resolve;
+    script.onerror = reject;
+
+    document.head.appendChild(script);
+  })
+};
+
+async function main() {
+  await loadScript("_ch-highlight.js");
+  langs = [
+    "armasm",
+    "avrasm",
+    "bash",
+    "c",
+    "clojure",
+    "coq",
+    "cpp",
+    "csharp",
+    "css",
+    "go",
+    "groovy",
+    "haskell",
+    "java",
+    "javascript",
+    "kotlin",
+    "latex",
+    "lisp",
+    "php",
+    "protobuf",
+    "python",
+    "python-repl",
+    "r",
+    "ruby",
+    "rust",
+    "scss",
+    "shell",
+    "sql",
+    "swift",
+    "typescript",
+    "vbnet",
+    "vim",
+    "wasm",
+    "yaml",
+    "xml"];
+  await Promise.allSettled(
+    langs.map((lang) => loadScript("_ch-hljs-lang-" + lang + ".min.js")))
+  hljs.highlightAll();
+};
+main();
+
